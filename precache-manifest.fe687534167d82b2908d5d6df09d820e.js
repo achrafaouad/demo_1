06e43f85df25ac052d89a5c3ff30f65fec8cef8d
achrafaouad <achrafaouad7@gmail.com>
2021-08-05 17:25:51 +0100

    Updates

---
+++ precache-manifest.fe687534167d82b2908d5d6df09d820e.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21b52bb60e173c8507fd35600e5572e1",
+    "url": "/demo_1/index.html"
+  },
+  {
+    "revision": "4a08174cd3ca92607620",
+    "url": "/demo_1/static/css/3.e64b7dab.chunk.css"
+  },
+  {
+    "revision": "bf0150ca9fa04909be87",
+    "url": "/demo_1/static/css/4.58abcbf4.chunk.css"
+  },
+  {
+    "revision": "3ba40242ebb14f24491a",
+    "url": "/demo_1/static/css/main.e14222dd.chunk.css"
+  },
+  {
+    "revision": "7264020f4a419b12b5bf",
+    "url": "/demo_1/static/js/10.0949f5aa.chunk.js"
+  },
+  {
+    "revision": "4ee485161fb460250574",
+    "url": "/demo_1/static/js/11.52144dc7.chunk.js"
+  },
+  {
+    "revision": "468641e98a5e9fb739ca",
+    "url": "/demo_1/static/js/12.50b468cd.chunk.js"
+  },
+  {
+    "revision": "53cbdf8d838c3d739e8f",
+    "url": "/demo_1/static/js/13.52380c72.chunk.js"
+  },
+  {
+    "revision": "f03f78b33dc68439a3c5",
+    "url": "/demo_1/static/js/14.1244c7b3.chunk.js"
+  },
+  {
+    "revision": "f165ecc1bb079baee028",
+    "url": "/demo_1/static/js/15.0cb61a6f.chunk.js"
+  },
+  {
+    "revision": "cb58914e6c38174de6f2",
+    "url": "/demo_1/static/js/16.44bda5fe.chunk.js"
+  },
+  {
+    "revision": "f2a611d01ab182c4b4a8",
+    "url": "/demo_1/static/js/17.d526b6f7.chunk.js"
+  },
+  {
+    "revision": "03337fb876e64b3ecd96",
+    "url": "/demo_1/static/js/2.9e609fae.chunk.js"
+  },
+  {
+    "revision": "4a08174cd3ca92607620",
+    "url": "/demo_1/static/js/3.f65d152e.chunk.js"
+  },
+  {
+    "revision": "bf0150ca9fa04909be87",
+    "url": "/demo_1/static/js/4.a075925e.chunk.js"
+  },
+  {
+    "revision": "5a6ee3d09c75f1217afa",
+    "url": "/demo_1/static/js/5.28f9c548.chunk.js"
+  },
+  {
+    "revision": "d64fa70a44a67c7563ce",
+    "url": "/demo_1/static/js/6.931ecd59.chunk.js"
+  },
+  {
+    "revision": "b782aa43cbcf0e5febc9",
+    "url": "/demo_1/static/js/7.2066e9a3.chunk.js"
+  },
+  {
+    "revision": "ad7dcd68ded9722f368a",
+    "url": "/demo_1/static/js/8.dda90ebf.chunk.js"
+  },
+  {
+    "revision": "c4ae81306f67502f5a06",
+    "url": "/demo_1/static/js/9.2bd0d4d1.chunk.js"
+  },
+  {
+    "revision": "3ba40242ebb14f24491a",
+    "url": "/demo_1/static/js/main.3cbf3001.chunk.js"
+  },
+  {
+    "revision": "e24c1075f9965835bce6",
+    "url": "/demo_1/static/js/runtime~main.7e4cd812.js"
+  },
+  {
+    "revision": "42d41e6133b1ff5a44c537ea957a7ab9",
+    "url": "/demo_1/static/media/face1.42d41e61.jpg"
+  },
+  {
+    "revision": "736ec0d93068fe61207fccff789a7e2e",
+    "url": "/demo_1/static/media/face15.736ec0d9.jpg"
+  },
+  {
+    "revision": "7e0e382da357aafeebaa825400bb590b",
+    "url": "/demo_1/static/media/face2.7e0e382d.jpg"
+  },
+  {
+    "revision": "16c67435545cf63033bb5876a2736a9c",
+    "url": "/demo_1/static/media/face3.16c67435.jpg"
+  },
+  {
+    "revision": "d5afaa667746492dad279ef18667e623",
+    "url": "/demo_1/static/media/face4.d5afaa66.jpg"
+  },
+  {
+    "revision": "d24172840592b28bba6872ffce4e2843",
+    "url": "/demo_1/static/media/face5.d2417284.jpg"
+  },
+  {
+    "revision": "07adc9a9ac3f0a888806a9071a8bf8f6",
+    "url": "/demo_1/static/media/face6.07adc9a9.jpg"
+  },
+  {
+    "revision": "7af91f951b43200c0786517742caf90e",
+    "url": "/demo_1/static/media/face7.7af91f95.jpg"
+  },
+  {
+    "revision": "0d7ef3668eba8487d2a5a1dbae48c3c0",
+    "url": "/demo_1/static/media/lockscreen-bg.0d7ef366.jpg"
+  },
+  {
+    "revision": "2eeb0447c0350018da406e77dfec1f10",
+    "url": "/demo_1/static/media/login-bg.2eeb0447.jpg"
+  },
+  {
+    "revision": "cc90dde4c908b222c6b142eca3aec5ce",
+    "url": "/demo_1/static/media/loginImg.cc90dde4.jpg"
+  },
+  {
+    "revision": "c949e51e2ffda20c554607fa3de32e87",
+    "url": "/demo_1/static/media/logo-mini.c949e51e.svg"
+  },
+  {
+    "revision": "8d2895f59bda26b31692990d033f0249",
+    "url": "/demo_1/static/media/logo.8d2895f5.svg"
+  },
+  {
+    "revision": "3ac50b5b36eb2f11b000dce1792d0bb0",
+    "url": "/demo_1/static/media/materialdesignicons-webfont.3ac50b5b.ttf"
+  },
+  {
+    "revision": "7ec5dab7e7ff250971d2ff50379778dc",
+    "url": "/demo_1/static/media/materialdesignicons-webfont.7ec5dab7.woff2"
+  },
+  {
+    "revision": "a0d13d16cc2f3647680d9f1ff003f58b",
+    "url": "/demo_1/static/media/materialdesignicons-webfont.a0d13d16.woff"
+  },
+  {
+    "revision": "a32fa1f27abbfa96ff2f79e1ade723d5",
+    "url": "/demo_1/static/media/materialdesignicons-webfont.a32fa1f2.eot"
+  },
+  {
+    "revision": "2ccde6b9925e2d16454cca89664a03e5",
+    "url": "/demo_1/static/media/register-bg.2ccde6b9.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/demo_1/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/demo_1/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/demo_1/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/demo_1/static/media/slick.f97e3bbf.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/demo_1/precache-manifest.fe687534167d82b2908d5d6df09d820e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/demo_1/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
